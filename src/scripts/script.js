f855f26ef8db03f8336d2a427fbe311bd704f7f4
Wynne <76490156+wwyynnee@users.noreply.github.com>
2022-08-02 17:41:12 +0000

    Исправление недочётов, улучшение качества кода

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -10,3 +10,13 @@ const closeMenu = () => {
   $(".header__active").css("backgroundColor", "transparent");
   $(".header__menu").hide();
 }
+
+// Ссылки на странице Условий Использования -> terms.html
+$(".main__link-terms").hide();
+
+$("#terms" || "#use-license" || "#disclaimer" || "#limitations" || "#accuracy" || "#links" || "#modifications" || "#law").on("mouseenter", () => {
+  $("#terms .main__link-terms").show();
+})
+$(".main__title-terms").on("mouseleave", () => {
+  $(".main__link-terms").hide();
+})
