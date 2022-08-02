6a82378f5ece207283a4fe6060d34cba04a6cde4
Wynne <76490156+wwyynnee@users.noreply.github.com>
2022-08-02 19:09:27 +0000

    Изменение файлов и улучшение структуры

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,21 +1,46 @@
 // Блок выбора языка
 $(".header__menu").hide();
 
-const openMenu = () => {
+$(".header__active").on("click", () => {
   $(".header__active").css("backgroundColor", "#ffffff10");
-  $(".header__menu").show();
-}
+  $(".header__active").css("borderRadius", "6px");
+  $(".header__menu").toggle();
+})
 
-const closeMenu = () => {
+$("*").on("click", (e) => {
+  if ($(e.target).is(".header__active")) {
+    return false;
+  }
   $(".header__active").css("backgroundColor", "transparent");
   $(".header__menu").hide();
-}
+})
 
 // Ссылки на странице Условий Использования -> terms.html
 $(".main__link-terms").hide();
 
-$("#terms" || "#use-license" || "#disclaimer" || "#limitations" || "#accuracy" || "#links" || "#modifications" || "#law").on("mouseenter", () => {
+$("#terms").on("mouseenter", () => {
   $("#terms .main__link-terms").show();
+})
+$("#use-license").on("mouseenter", () => {
+  $("#use-license .main__link-terms").show();
+})
+$("#disclaimer").on("mouseenter", () => {
+  $("#disclaimer .main__link-terms").show();
+})
+$("#limitations").on("mouseenter", () => {
+  $("#limitations .main__link-terms").show();
+})
+$("#accuracy").on("mouseenter", () => {
+  $("#accuracy .main__link-terms").show();
+})
+$("#links").on("mouseenter", () => {
+  $("#links .main__link-terms").show();
+})
+$("#modifications").on("mouseenter", () => {
+  $("#modifications .main__link-terms").show();
+})
+$("#law").on("mouseenter", () => {
+  $("#law .main__link-terms").show();
 })
 $(".main__title-terms").on("mouseleave", () => {
   $(".main__link-terms").hide();
