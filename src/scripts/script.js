4e94529169951d0aa107eab7f9c2063b463bd794
Wynne <76490156+wwyynnee@users.noreply.github.com>
2022-08-03 07:48:13 +0000

    Исправление ошибок

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,47 +1,46 @@
 // Блок выбора языка
 $(".header__menu").hide();
 
-$(".header__active, .header__icon-links, .header__text-links, .header__megalog, .header__little-log").on("click", () => {
+let active = ".header__active, .header__block-active";
+
+$(`${active}`).on("click", () => {
   $(".header__active").css("backgroundColor", "#ffffff10");
   $(".header__active").css("borderRadius", "6px");
   $(".header__menu").toggle();
 })
 
 $("*").on("click", (e) => {
-if ($(e.target).is(".header__active, .header__icon-links, .header__text-links, .header__megalog, .header__little-log")) {
+  if ($(e.target).is(`${active}`)) {
     return false;
   }
   $(".header__active").css("backgroundColor", "transparent");
   $(".header__menu").hide();
 })
 
-// Ссылки на странице Условий Использования -> terms.html
-$(".main__link-terms").hide();
+// Смена темы
+const btn = document.getElementById("theme-button");
+const link = document.getElementById("theme-link");
 
-$("#terms").on("mouseenter", () => {
-  $("#terms .main__link-terms").show();
-})
-$("#use-license").on("mouseenter", () => {
-  $("#use-license .main__link-terms").show();
-})
-$("#disclaimer").on("mouseenter", () => {
-  $("#disclaimer .main__link-terms").show();
-})
-$("#limitations").on("mouseenter", () => {
-  $("#limitations .main__link-terms").show();
-})
-$("#accuracy").on("mouseenter", () => {
-  $("#accuracy .main__link-terms").show();
-})
-$("#links").on("mouseenter", () => {
-  $("#links .main__link-terms").show();
-})
-$("#modifications").on("mouseenter", () => {
-  $("#modifications .main__link-terms").show();
-})
-$("#law").on("mouseenter", () => {
-  $("#law .main__link-terms").show();
-})
-$(".main__title-terms").on("mouseleave", () => {
-  $(".main__link-terms").hide();
-})
+btn.addEventListener("click", () => {
+  ChangeTheme();
+});
+
+const ChangeTheme = () => {
+  let lightTheme = "src/styles/light.css";
+  let darkTheme = "src/styles/dark.css";
+
+  let currTheme = link.getAttribute("href");
+  let theme;
+
+  if (currTheme == lightTheme) {
+    currTheme = darkTheme;
+    theme = "dark";
+  } else {
+    currTheme = lightTheme;
+    theme = "light";
+  }
+
+  link.setAttribute("href", currTheme);
+
+  Save(theme);
+}
